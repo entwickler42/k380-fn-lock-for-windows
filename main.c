46b45aca662d7c965e4526389eafea5e63fc4b75
Ralf Richter <entwickler42@gmail.com>
2023-06-22 08:43:05 +0700

    C++ & Command line arguments instead of macros

--- main.c
+++ main.c
@@ -23,55 +23,72 @@ values from : https://github.com/jergusg/k380-function-keys-conf/blob/master/k38
 
 */
 
-#include <stdio.h> // printf
-#include <wchar.h> // wchar_t
-
+#include <iostream>
+#include <cstring>
 #include <hidapi.h>
+#include <unistd.h>
 
 #define MAX_STR 255
 
 int main(int argc, char *argv[])
 {
     int seq_len = 7;
+
     const unsigned char k380_seq_fkeys_on[] = {0x10, 0xff, 0x0b, 0x1e, 0x00, 0x00, 0x00};
     const unsigned char k380_seq_fkeys_off[] = {0x10, 0xff, 0x0b, 0x1e, 0x01, 0x00, 0x00};
+    
     int k380_vid = 0x46d;
     int k380_pid = 0xb342;
     static const int TARGET_USAGE = 1;
     static const int TARGET_USAGE_PAGE = 65280;
-
+    
     int res;
     int result = -1;
-    unsigned char buf[65];
-    wchar_t wstr[MAX_STR];
     hid_device *handle;
     struct hid_device_info *devs, *cur_dev;
-
+    
+    // Parse command-line options
+    int opt;
+    bool setMediaKeys = false;
+    while ((opt = getopt(argc, argv, "m")) != -1) {
+        switch (opt) {
+            case 'm':
+                setMediaKeys = true;
+                break;
+            default:
+                std::cerr << "Invalid command-line option" << std::endl;
+                return 1;
+        }
+    }
+    
     // Initialize the hidapi library
     res = hid_init();
-
+    
     devs = hid_enumerate(k380_vid, k380_pid);
     cur_dev = devs;
-
+    
     while (cur_dev)
     {
         if (cur_dev->usage == TARGET_USAGE && cur_dev->usage_page == TARGET_USAGE_PAGE)
         {
             handle = hid_open_path(cur_dev->path);
-
-#ifdef setMediaKeys
-            printf("Set media keys as default");
-            res = hid_write(handle, k380_seq_fkeys_off, seq_len);
-#else
-            printf("Set function keys as default");
-            res = hid_write(handle, k380_seq_fkeys_on, seq_len);
-#endif
-
-            if (res != seq_len)
+            
+            if (setMediaKeys)
             {
-                printf("error: %ls\n", hid_error(handle));
+                std::cout << "Set media keys as default" << std::endl;
+                res = hid_write(handle, k380_seq_fkeys_off, seq_len);
             }
-
+            else
+            {
+                std::cout << "Set function keys as default" << std::endl;
+                res = hid_write(handle, k380_seq_fkeys_on, seq_len);
+            }
+            
+            if (res != seq_len){
+                std::cout << "error:";
+                std::wcout << hid_error(handle) << std::endl;
+            }
+            
             hid_close(handle);
             if (res < 0)
             {
@@ -86,8 +103,9 @@ int main(int argc, char *argv[])
         }
         cur_dev = cur_dev->next;
     }
+    
     hid_free_enumeration(devs);
-
     hid_exit();
+    
     return result;
 }
